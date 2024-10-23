434052086b8c2a4df9ad2be76a22d6ac37205351
vaibhav <vaibhav@strative.ai>
2024-10-23 12:36:29 -0400

    update mmluqa.py

--- mmluqa.py
+++ mmluqa.py
@@ -4,6 +4,7 @@ import re
 import torch
 import time
 import gc
+import json
 
 from src.utils1 import QADataset,locate_answer,locate_answer4pub_llama
 # from torch.utils.data import Dataset, DataLoader
@@ -16,16 +17,16 @@ pmc_llama = 'axiong/PMC_LLaMA_13B'
 
 medrag = MedRAG(llm_name=pmc_llama, rag=True, retriever_name="RRF-4", corpus_name="MedCorp")
 
-desired_subjects = ['anatomy', 'clinical_knowledge', 'professional_medicine', 'human_genetics', 'college_medicine', 'college_biology']
-dataset = load_dataset('cais/mmlu', "all")
+# desired_subjects = ['anatomy', 'clinical_knowledge', 'professional_medicine', 'human_genetics', 'college_medicine', 'college_biology']
+# dataset = load_dataset('cais/mmlu', "all")
 
-def filter_and_convert_to_pandas(ds, desired_subjects):
-        # Filter the dataset
-        filtered_ds = ds.filter(lambda row: row['subject'] in desired_subjects)
-        # Convert the filtered dataset to Pandas DataFrame
-        return filtered_ds.to_pandas()
+# def filter_and_convert_to_pandas(ds, desired_subjects):
+#         # Filter the dataset
+#         filtered_ds = ds.filter(lambda row: row['subject'] in desired_subjects)
+#         # Convert the filtered dataset to Pandas DataFrame
+#         return filtered_ds.to_pandas()
 
-filtered_datasets = {}
+# filtered_datasets = {}
 
 
 # def process_batch(batch_df):
@@ -72,61 +73,106 @@ filtered_datasets = {}
 #         p.start()
 #         p.join()  # Wait for the process to finish before starting the next one
 #     #``
-from multiprocessing import Process, set_start_method
+# from multiprocessing import Process, set_start_method
 
-def process_batch(batch_df, device_id):
-    torch.cuda.set_device(device_id)
+# def process_batch(batch_df, device_id):
+#     torch.cuda.set_device(device_id)
+#     count = 0
+#     for index, row in batch_df.iterrows():
+#         question = row['question']  # Adjust column name as necessary
+#         options = {chr(65 + i): option for i, option in enumerate(row['choices'])}
+#         torch.cuda.empty_cache()
+#         gc.collect()
+#         answer, snippets, scores = medrag.answer(question=question, options=options, k=32)
+#         print(answer)
+#         choice = locate_answer4pub_llama(answer)
+#         value = row['answer']
+#         if choice == str(chr(65 + value)):
+#             count += 1
+#         print(f'Index: {index}, Extracted answer: {choice},  actual answer: {str(chr(65 + value))} , score: {scores}')
+#         time.sleep(1)
+#     print(f'Batch processed on device {device_id}. Correct answers: {count}')
+
+
+# def process_batches(batches, device_id):
+#     import torch
+#     torch.cuda.set_device(device_id)
+#     # Initialize medrag or any other necessary resources here
+#     for batch_df in batches:
+#         process_batch(batch_df, device_id)
+
+# if __name__ == "__main__":
+#     try:
+#         set_start_method('spawn', force=True)  # Ensure 'spawn' is used for multiprocessing
+#     except RuntimeError:
+#         pass
+
+#     for split in dataset:
+#         print(f"Processing {split} split...")
+#         # Filter and convert each split to a DataFrame
+#         filtered_datasets[split] = filter_and_convert_to_pandas(dataset[split], desired_subjects)
+
+#     filtered_test_df = filtered_datasets['test']
+#     # Assuming 'filtered_test_df' is your DataFrame and 'batch_size' is defined
+#     batch_size = 150
+#     batches = [filtered_test_df.iloc[i:i+batch_size] for i in range(0, len(filtered_test_df), batch_size)]
+
+#     device_ids = [0]  # IDs of your GPUs
+#     processes = []
+#     timeStart = time.time()
+#     for i, device_id in enumerate(device_ids):
+#         # Assign batches to this device: every nth batch starting from i
+#         device_batches = batches[i::len(device_ids)]
+#         p = Process(target=process_batches, args=(device_batches, device_id))
+#         p.start()
+#         processes.append(p)
+
+#     for p in processes:
+#         p.join()
+#     timeEnd = time.time()
+#     print(timeEnd-timeStart)
+
+def extract_data(data):
+    extracted_data = []
+    
+    for key, value in data.items():
+        question = value.get('question')
+        options = value.get('options')
+        answer = value.get('answer')
+        
+        extracted_data.append({
+            'question': question,
+            'options': options,
+            'answer': answer
+        })
+    
+    return extracted_data
+
+pmc_llama = 'axiong/PMC_LLaMA_13B'
+
+if __name__ == "__main__":
+    medrag = MedRAG(llm_name=pmc_llama, rag=False)
+    with open('benchmark.json', 'r') as file:
+        data = json.load(file)
+    
+    medqa = data['mmlu']
+
+    extracted_data = extract_data(medqa)
     count = 0
-    for index, row in batch_df.iterrows():
-        question = row['question']  # Adjust column name as necessary
-        options = {chr(65 + i): option for i, option in enumerate(row['choices'])}
+    for item in extracted_data:
+        question = item['question']
+        options = item['options'] 
         torch.cuda.empty_cache()
         gc.collect()
         answer, snippets, scores = medrag.answer(question=question, options=options, k=32)
         print(answer)
         choice = locate_answer4pub_llama(answer)
-        value = row['answer']
-        if choice == str(chr(65 + value)):
+        value = item['answer']
+        if choice == str(item['answer']):
             count += 1
-        print(f'Index: {index}, Extracted answer: {choice},  actual answer: {str(chr(65 + value))} , score: {scores}')
-        time.sleep(1)
-    print(f'Batch processed on device {device_id}. Correct answers: {count}')
+        print(f"Expected Answer: {choice} , Actual Answer: {value}")
+    
+    print(f"The number of correct answers: {count}")
+    
 
-
-def process_batches(batches, device_id):
-    import torch
-    torch.cuda.set_device(device_id)
-    # Initialize medrag or any other necessary resources here
-    for batch_df in batches:
-        process_batch(batch_df, device_id)
-
-if __name__ == "__main__":
-    try:
-        set_start_method('spawn', force=True)  # Ensure 'spawn' is used for multiprocessing
-    except RuntimeError:
-        pass
-
-    for split in dataset:
-        print(f"Processing {split} split...")
-        # Filter and convert each split to a DataFrame
-        filtered_datasets[split] = filter_and_convert_to_pandas(dataset[split], desired_subjects)
-
-    filtered_test_df = filtered_datasets['test']
-    # Assuming 'filtered_test_df' is your DataFrame and 'batch_size' is defined
-    batch_size = 150
-    batches = [filtered_test_df.iloc[i:i+batch_size] for i in range(0, len(filtered_test_df), batch_size)]
-
-    device_ids = [0]  # IDs of your GPUs
-    processes = []
-    timeStart = time.time()
-    for i, device_id in enumerate(device_ids):
-        # Assign batches to this device: every nth batch starting from i
-        device_batches = batches[i::len(device_ids)]
-        p = Process(target=process_batches, args=(device_batches, device_id))
-        p.start()
-        processes.append(p)
-
-    for p in processes:
-        p.join()
-    timeEnd = time.time()
-    print(timeEnd-timeStart)
+    
